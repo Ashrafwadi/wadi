47e30226f0a10f65d9b86525116790c0446c4473
Wadi <wadi@fasttelco.net>
2012-11-22 13:09:21 +0300

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WadiDemo::Application.config.secret_token = 'cbb3a339443a6f90666b69fdfc0620fd09cffa99f9d795864af568e99f57db4faa1acbfc006d9f9fd95205d1b80887fd7e5050e2382247d6f558af07ec7e7fd0'
